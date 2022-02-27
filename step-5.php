42df570d0f067ffd16ce24d8c174abb5bbd7ebe9
wissal <wissalalhj@gmail.com>
2022-02-27 16:45:55 +0200

    Basic-1/Step-5

---
+++ step-5.php
@@ -0,0 +1,11 @@
+<?php
+$course_name="Rust, the language of no bugs";
+$enrolled_students=20;
+$price=30;
+$on_discount=($course_name) ? true : false;
+echo"
+Course title: $course_name <br>
+Enrolled Students: $enrolled_students <br>
+Course price: $price $ USD <br>
+Course on discount:$on_discount";
+?>
