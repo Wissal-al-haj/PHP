84228bddbe07873c49ebb01a38d8ddfca8c2665e
wissal <wissalalhj@gmail.com>
2022-02-27 16:29:18 +0200

    Basic-1/Step-4

---
+++ step-4.php
@@ -0,0 +1,17 @@
+<?php
+$name="Wissal";
+$position="Student";
+$github_url="https://github.com/wissal-al-haj";
+//1
+echo"Hello I am ". $name ." , I am ". $position ." please check my github link ".$github_url;
+echo"<br>";
+//2
+echo"Hello I am $name , I am $position please check my github link $github_url";
+echo"<br>";
+//3
+echo'Hello I am '.$name.' , I am '.$position.' please check my github link '. $github_url ;
+echo"<br>";
+//4
+echo'Hello I am ',$name,' , I am ',$position,' please check my github link ',$github_url ;
+echo"<br>";
+?>
