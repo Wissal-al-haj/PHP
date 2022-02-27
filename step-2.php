d4ee313ce48695a849dfef5eea6c3243b1d06cfd
wissal <wissalalhj@gmail.com>
2022-02-27 15:43:32 +0200

    Basic-1/Step-3

--- step-2.php
+++ step-2.php
@@ -1,6 +1,7 @@
-Hello world, Help me I am using PHP
+Hello world, Help me I am using PHP<br>
 <?php
-print"I am getting errors but I cant see the errors\n";
+print"I am getting errors but I cant see the errors";
+echo"<br>";
 echo "Search on how to enable PHP Errors";
 ?>
 
---
+++ step-3.php
@@ -0,0 +1,10 @@
+<?php
+$name="Wissal";
+$email="wissalalhj@gmail.com";
+$height=156;
+$width=36;
+$area=$height*$width;
+echo"My name is: $name  and  my email is: $email";
+echo"<br>";
+echo "Area = $area";
+?>
